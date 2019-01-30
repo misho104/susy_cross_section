69bde8e9e1e54d7135e1fb73c27e7c68c8603400
Sho Iwamoto <contact@misho-web.com>
2019-01-30 19:59:42 +0100

    data: LHC SUSY XS WG slepton and x1+x1-

--- contrib/get_lhc_susy_xs_wg.sh
+++ contrib/get_lhc_susy_xs_wg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SCRAPER=./scraper.py
-OUTPUT_DIR="./lhc_susy_xs_wg"
+OUTPUT_DIR="../susy_cross_section/data/lhc_susy_xs_wg"
 
 mkdir -p $OUTPUT_DIR
 
@@ -15,5 +15,15 @@ cat tmp | python $SCRAPER --index=6 > $OUTPUT_DIR/13TeVn2x1wino_cteq_m.csv
 cat tmp | python $SCRAPER --index=7 > $OUTPUT_DIR/13TeVn2x1wino_mstw_pm.csv
 cat tmp | python $SCRAPER --index=8 > $OUTPUT_DIR/13TeVn2x1wino_mstw_p.csv
 cat tmp | python $SCRAPER --index=9 > $OUTPUT_DIR/13TeVn2x1wino_mstw_m.csv
+
+curl -o tmp "https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVx1x1wino"
+cat tmp | python $SCRAPER --index=1 > $OUTPUT_DIR/13TeVx1x1wino_envelope.csv
+cat tmp | python $SCRAPER --index=2 > $OUTPUT_DIR/13TeVx1x1wino_cteq.csv
+cat tmp | python $SCRAPER --index=3 > $OUTPUT_DIR/13TeVx1x1wino_mstw.csv
+
+curl -o tmp "https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVslepslep"
+cat tmp | python $SCRAPER --index=1 > $OUTPUT_DIR/13TeVslepslep_ll.csv
+cat tmp | python $SCRAPER --index=2 > $OUTPUT_DIR/13TeVslepslep_rr.csv
+cat tmp | python $SCRAPER --index=3 > $OUTPUT_DIR/13TeVslepslep_maxmix.csv
 
 rm tmp
--- susy_cross_section/config.py
+++ susy_cross_section/config.py
@@ -6,4 +6,8 @@ table_names = {
     '13TeV.n2x1-.wino': 'data/lhc_susy_xs_wg/13TeVn2x1wino_envelope_m.csv',
     '13TeV.n2x1+.wino': 'data/lhc_susy_xs_wg/13TeVn2x1wino_envelope_p.csv',
     '13TeV.n2x1+-.wino': 'data/lhc_susy_xs_wg/13TeVn2x1wino_envelope_pm.csv',
+    '13TeV.x1x1.wino': 'data/lhc_susy_xs_wg/13TeVx1x1wino_envelope.csv',
+    '13TeV.slepslep.ll': 'data/lhc_susy_xs_wg/13TeVslepslep_ll.csv',
+    '13TeV.slepslep.rr': 'data/lhc_susy_xs_wg/13TeVslepslep_rr.csv',
+    '13TeV.slepslep.maxmix': 'data/lhc_susy_xs_wg/13TeVslepslep_maxmix.csv',
 }  # type: Mapping[str, Union[str, Tuple[str, str]]]
