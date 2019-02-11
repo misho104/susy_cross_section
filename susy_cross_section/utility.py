b549ce135b908181afac4ecc208fc8227afab8d7
Sho Iwamoto <contact@misho-web.com>
2019-02-11 03:06:51 +0100

    refactor: simplify Unit methods with more docstring

--- susy_cross_section/utility.py
+++ susy_cross_section/utility.py
@@ -12,61 +12,126 @@ if sys.version_info[0] < 3:  # py2
 
 
 class Unit:
-    """The unit of a physical value.
+    """A class to handle units of physical values.
 
-    The constructor must be called with units, where units must be str.
+    This class handles units associated to physical values. Units can be
+    multiplied, inverted, or converted. A new instance is equivalent to the
+    product of `!*args`; each argument can be a str, a Unit, or a float (as a
+    numerical factor).
+
+    Parameters
+    ----------
+    *args: float, str, or Unit
+        Factors of the new instance.
     """
 
     definitions = {
         '': [1],
         '%': [0.01],
         'pb': [1000, 'fb'],
-    }  # type: Mapping[Union[float, str], List[Union[float, str]]]
+    }  # type: Mapping[str, List[Union[float, str]]]
+    """:typ:`dict[str, list of (float or str)]`: The replacement rules of units.
+
+    This dictionary defines the replacement rules for unit conversion.
+    Each key should be replaced with the product of its values."""
 
     def __init__(self, *args):
         # type: (Union[float, str, Unit])->None
-        self.factor = 1   # type: float
-        self.units = {}   # type: MutableMapping[str, int]
+        self._factor = 1   # type: float
+        self._units = {}   # type: MutableMapping[str, int]
         for u in args:
-            if isinstance(u, Unit):
-                self.factor *= u.factor
-                for k, v in u.units.items():
-                    self.units[k] = self.units.get(k, 0) + v
-            else:
-                base_units = self.definitions.get(u, [u])
-                for b in base_units:
-                    if isinstance(b, str):
-                        self.units[b] = self.units.get(b, 0) + 1
-                    else:
-                        try:
-                            self.factor *= float(b)
-                        except ValueError:
-                            raise TypeError('invalid unit: %s', u)
+            self.__imul__(u)
 
-    def invert(self):
+    def inverse(self):
         # type: ()->Unit
-        """Return an inverted unit."""
+        """Return an inverted unit.
+
+        Returns
+        -------
+        Unit
+            The inverted unit of `!self`.
+        """
         result = Unit()
-        result.factor = 1 / self.factor
-        result.units = {k: -v for k, v in self.units.items()}
+        result._factor = 1 / self._factor
+        result._units = {k: -v for k, v in self._units.items()}
         return result
+
+    def __imul__(self, other):
+        # type: (Union[float, str, Unit])->None
+        """Multiply by another unit.
+
+        Parameters
+        ----------
+        other: float, str, or Unit
+            Another unit as a multiplier.
+        """
+        if isinstance(other, Unit):
+            self._factor *= other._factor
+            for k, v in other._units.items():
+                self._units[k] = self._units.get(k, 0) + v
+        else:
+            if isinstance(other, str):
+                base_units = self.definitions.get(other, [other])
+            else:
+                base_units = [other]
+            for b in base_units:
+                if isinstance(b, str):
+                    self._units[b] = self._units.get(b, 0) + 1
+                else:
+                    try:
+                        self._factor *= float(b)
+                    except ValueError:
+                        raise TypeError('invalid unit: %s', other)
 
     def __mul__(self, other):
         # type: (Union[float, str, Unit])->Unit
-        """Return products of two units."""
+        """Return products of two units.
+
+        Parameters
+        ----------
+        other: float, str, or Unit
+            Another unit as a multiplier.
+
+        Returns
+        -------
+        Unit
+            The product.
+        """
         return Unit(self, other)
 
     def __truediv__(self, other):
         # type: (Union[float, str, Unit])->Unit
-        """Return division of two units."""
-        return self * Unit(other).invert()
+        """Return division of two units.
+
+        Parameters
+        ----------
+        other: float, str, or Unit
+            Another unit as a divider.
+
+        Returns
+        -------
+        Unit
+            The quotient.
+        """
+        return Unit(self, Unit(other).inverse())
 
     def __float__(self):
         # type: ()->float
-        """Return factor if it is without units."""
-        if any(v != 0 for v in self.units.values()):
-            raise ValueError('Unit conversion error: %s, %s', self.units, self.factor)
-        return float(self.factor)
+        """Evaluate as a float value if this is a dimension-less unit.
+
+        Returns
+        -------
+        float
+            The number corresponding to this dimension-less unit.
+
+        Raises
+        ------
+        ValueError
+            If not dimension-less unit.
+        """
+        if any(v != 0 for v in self._units.values()):
+            raise ValueError('Unit conversion error: %s, %s', self._units, self._factor)
+        return float(self._factor)
 
 
 def value_format(value, unc_p, unc_m, unit=None):
