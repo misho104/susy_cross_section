c875450e7e87ea5267a7de7a5ad145e27be7a1ac
Sho Iwamoto <contact@misho-web.com>
2019-03-01 13:31:57 +0100

    doc: bibtex configuration

--- docs/_themes/conf_bibtex.py
+++ docs/_themes/conf_bibtex.py
@@ -1,6 +1,9 @@
 # cSpell:words pybtex unsrt arxiv eprint
+import re
 
-from pybtex.plugin import register_plugin
+import pybtex.plugin
+import pybtex.richtext
+import pybtex.style.labels.alpha
 from pybtex.style.formatting import toplevel
 from pybtex.style.formatting.unsrt import Style as UnsrtStyle
 from pybtex.style.formatting.unsrt import date, pages
@@ -19,6 +22,30 @@ from pybtex.style.template import (  # noqa: F401
 )
 
 
+def format_collaboration(entry, for_label=False):
+    collaboration = entry.fields.get("collaboration")
+    if collaboration:
+        if collaboration.lower().endswith(" collaboration"):
+            collaboration = collaboration[:-14]
+    return collaboration
+
+
+class MylabelStyle(pybtex.style.labels.alpha.LabelStyle):
+    def author_key_label(self, entry):
+        # overwrite by collaboration
+        collaboration = format_collaboration(entry)
+        if collaboration:
+            collaboration = re.sub(r"[^A-Za-z0-9]", "", collaboration)
+            return collaboration[:4]
+        return super().author_key_label(entry)
+
+    def format_labels(self, sorted_entries):
+        return super().format_labels(sorted_entries)
+
+    def format_label(self, entry):
+        return super().format_label(entry)
+
+
 class MyStyle(UnsrtStyle):
     default_sorting_style = "author_year_title"
     default_label_style = "alpha"
@@ -26,11 +53,21 @@ class MyStyle(UnsrtStyle):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.abbreviate_names = True
+        self.label_style = MylabelStyle()
+        self.format_labels = self.label_style.format_labels
+
+    def format_author_or_editor(self, e):
+        collaboration = format_collaboration(e)
+        if collaboration:
+            return join[tag("strong")[collaboration], " Collaboration"]
+
+        return first_of[optional[self.format_names("author")], self.format_editor(e)]
 
     def format_title(self, e, which_field, as_sentence=True):
-        formatted_title = tag("em")[
-            field(which_field, apply_func=lambda text: text.capitalize())
-        ]
+        title_math_match = re.compile("\$(.*?)\$")
+        title_raw = e.fields[which_field].lstrip("{").rstrip("}")
+        title = title_math_match.sub(r"\(\1\)", title_raw)
+        formatted_title = tag("em")[pybtex.richtext.Text(title)]
         if as_sentence:
             return sentence[formatted_title]
         else:
@@ -73,7 +110,7 @@ class MyStyle(UnsrtStyle):
         ]
 
         template = toplevel[
-            self.format_names("author"),
+            sentence[self.format_author_or_editor(e)],
             self.format_title(e, "title"),
             optional[sentence[journal]],
             sentence[optional_field("note")],
@@ -93,6 +130,7 @@ class MyStyle(UnsrtStyle):
         ]
 
         template = toplevel[
+            optional[sentence[self.format_author_or_editor(e)]],
             optional[sentence[self.format_names("author")]],
             optional[self.format_title(e, "title")],
             optional[howpublished],
@@ -100,8 +138,27 @@ class MyStyle(UnsrtStyle):
         ]
         return template
 
+    def get_techreport_template(self, e):
+        if "number" in e.fields and "url" in e.fields:
+            number = href[field("url", raw=True), field("number")]
+            linked = True
+        else:
+            number = optional_field("number")
+            linked = False
 
-register_plugin("pybtex.style.formatting", "default", MyStyle)
+        template = toplevel[
+            sentence[self.format_author_or_editor(e)],
+            self.format_title(e, "title"),
+            sentence[number, field("institution"), optional_field("address"), date],
+            sentence[optional_field("note")],
+        ]
+        if not linked:
+            template = template.append(self.format_web_refs(e))
+
+        return template
+
+
+pybtex.plugin.register_plugin("pybtex.style.formatting", "default", MyStyle)
 
 
 def setup(app):
